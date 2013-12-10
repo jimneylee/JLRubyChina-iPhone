70573a5a0c615bdf9d9b972c3e7809363e04e57b
jimneylee <jimney@163.com>
2013-12-10 17:20:54 +0800

    update

---
+++ JLRubyChinaTests/JLRubyChinaTests.m
@@ -0,0 +1,34 @@
+//
+//  JLRubyChinaTests.m
+//  JLRubyChinaTests
+//
+//  Created by ccjoy-jimneylee on 13-12-10.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JLRubyChinaTests : XCTestCase
+
+@end
+
+@implementation JLRubyChinaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
