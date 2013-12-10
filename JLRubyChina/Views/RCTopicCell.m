c9f06da6910d84f80ab3474b7b1e4f794a8a1158
jimneylee <jimney@163.com>
2013-12-11 00:24:28 +0800

    finish topic cell show, later on i will add NIAttributeLabel

--- JLRubyChina/Views/RCTopicCell.m
+++ JLRubyChina/Views/RCTopicCell.m
@@ -11,34 +11,39 @@
 #import "NimbusNetworkImage.h"
 #import "RCTopicEntity.h"
 
+#define NAME_FONT_SIZE [UIFont systemFontOfSize:15.f]
+#define DATE_FONT_SIZE [UIFont systemFontOfSize:12.f]
 #define TITLE_FONT_SIZE [UIFont systemFontOfSize:18.f]
-#define SUBTITLE_FONT_SIZE [UIFont systemFontOfSize:12.f]
-#define CONTENT_FONT_SIZE [UIFont systemFontOfSize:18.f]
+#define LAST_REPLIED_FONT_SIZE [UIFont systemFontOfSize:18.f]
 #define HEAD_IAMGE_HEIGHT 34
-#define SUBTITLE_
+
 @interface RCTopicCell()
-@property (nonatomic, strong) UILabel* contentLabel;
+@property (nonatomic, strong) UILabel* nameLabel;
+@property (nonatomic, strong) UILabel* dateLabel;
+@property (nonatomic, strong) UILabel* topicTitleLabel;
+@property (nonatomic, strong) UILabel* lastRepliedLabel;// todo use niattributelabel
 @property (nonatomic, strong) NINetworkImageView* headView;
 @end
+
 @implementation RCTopicCell
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 + (CGFloat)heightForObject:(id)object atIndexPath:(NSIndexPath *)indexPath tableView:(UITableView *)tableView
 {
     if ([object isKindOfClass:[RCTopicEntity class]]) {
-        CGFloat cellMargin = CELL_PADDING_6;
-        CGFloat contentViewMarin = CELL_PADDING_8;
+        CGFloat cellMargin = CELL_PADDING_4;
+        CGFloat contentViewMarin = CELL_PADDING_6;
         CGFloat sideMargin = cellMargin + contentViewMarin;
 
         CGFloat height = sideMargin;
         
         // head image
-//        height = height + HEAD_IAMGE_HEIGHT;
-//        height = height + CELL_PADDING_10;
+        height = height + HEAD_IAMGE_HEIGHT;
+        height = height + CELL_PADDING_4;
         
         RCTopicEntity* o = (RCTopicEntity*)object;
         
-        CGFloat kTitleLength = tableView.width - HEAD_IAMGE_HEIGHT - CELL_PADDING_10 -  sideMargin * 2;
+        CGFloat kTitleLength = tableView.width -  sideMargin * 2;
 //        NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:o.topicTitle];
 //        NSRange range = NSMakeRange(0, attrStr.length);
 //        NSDictionary* dic = [attrStr attributesAtIndex:0 effectiveRange:&range];   // 获取该段attributedString的属性字典
@@ -50,8 +55,8 @@
                                     constrainedToSize:CGSizeMake(kTitleLength, FLT_MAX)];
         height = height + titleSize.height;
         
-        //height = height + CELL_PADDING_10;
-        height = height + SUBTITLE_FONT_SIZE.lineHeight;
+        height = height + CELL_PADDING_4;
+        height = height + LAST_REPLIED_FONT_SIZE.lineHeight;
         
         height = height + sideMargin;
         
@@ -74,22 +79,27 @@
         [self.contentView addSubview:self.headView];
 
         // name
-        self.textLabel.numberOfLines = NO;
-        self.textLabel.font = TITLE_FONT_SIZE;
+        self.textLabel.font = NAME_FONT_SIZE;
         self.textLabel.textColor = [UIColor blackColor];
         self.textLabel.highlightedTextColor = self.textLabel.textColor;
         
         // source from & date
-        self.detailTextLabel.font = SUBTITLE_FONT_SIZE;
+        self.detailTextLabel.font = DATE_FONT_SIZE;
         self.detailTextLabel.textColor = [UIColor grayColor];
         self.detailTextLabel.highlightedTextColor = self.detailTextLabel.textColor;
         
-        // status content
-        self.contentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-        self.contentLabel.numberOfLines = 0;
-        self.contentLabel.font = CONTENT_FONT_SIZE;
-        self.contentLabel.textColor = [UIColor blackColor];
-        [self.contentView addSubview:self.contentLabel];
+        // topic title
+        self.topicTitleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        self.topicTitleLabel.numberOfLines = 0;
+        self.topicTitleLabel.font = TITLE_FONT_SIZE;
+        self.topicTitleLabel.textColor = [UIColor blackColor];
+        [self.contentView addSubview:self.topicTitleLabel];
+        
+        // topic title
+        self.lastRepliedLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        self.lastRepliedLabel.font = NAME_FONT_SIZE;
+        self.lastRepliedLabel.textColor = [UIColor grayColor];
+        [self.contentView addSubview:self.lastRepliedLabel];
         
         self.contentView.layer.borderColor = CELL_CONTENT_VIEW_BORDER_COLOR.CGColor;
         self.contentView.layer.borderWidth = 1.0f;
@@ -117,11 +127,11 @@
     self.contentView.backgroundColor = CELL_CONTENT_VIEW_BG_COLOR;
     self.textLabel.backgroundColor = [UIColor clearColor];
     self.detailTextLabel.backgroundColor = [UIColor clearColor];
-    self.contentLabel.backgroundColor = [UIColor clearColor];
+    self.topicTitleLabel.backgroundColor = [UIColor clearColor];
 
     // layout
-    CGFloat cellMargin = CELL_PADDING_6;
-    CGFloat contentViewMarin = CELL_PADDING_8;
+    CGFloat cellMargin = CELL_PADDING_4;
+    CGFloat contentViewMarin = CELL_PADDING_6;
     CGFloat sideMargin = cellMargin + contentViewMarin;
     
     self.contentView.frame = CGRectMake(cellMargin, cellMargin,
@@ -129,27 +139,27 @@
                                         self.height - cellMargin * 2);
     // head image
     self.headView.left = contentViewMarin;
-    self.headView.top = (self.height - self.headView.height) / 2;
+    self.headView.top = contentViewMarin;
     
     // name
-    CGFloat kTitleLength = self.contentView.width - contentViewMarin * 2 - (self.headView.right + CELL_PADDING_10);
-    CGSize titleSize = [self.textLabel.text sizeWithFont:TITLE_FONT_SIZE
-                                constrainedToSize:CGSizeMake(kTitleLength, FLT_MAX)];
-    self.textLabel.frame = CGRectMake(self.headView.right + CELL_PADDING_10, contentViewMarin,
-                                      kTitleLength,
-                                      titleSize.height);
+    // will - nodeNameButton
+    self.textLabel.frame = CGRectMake(self.headView.right + CELL_PADDING_6, contentViewMarin,
+                                      self.contentView.width - contentViewMarin * 2 - (self.headView.right + CELL_PADDING_6),
+                                      self.textLabel.font.lineHeight);
     
     // source from & date
-    self.detailTextLabel.frame = CGRectMake(self.textLabel.left, self.textLabel.bottom,
-                                            self.width - sideMargin * 2 - self.textLabel.left,
-                                            self.detailTextLabel.font.lineHeight);
+    self.detailTextLabel.frame = CGRectMake(self.textLabel.left, self.textLabel.bottom + CELL_PADDING_2,
+                                            self.textLabel.width, self.detailTextLabel.font.lineHeight);
     
     // status content
-//    CGFloat kContentLength = self.contentView.width - contentViewMarin * 2;
-//    CGSize contentSize = [self.contentLabel.text sizeWithFont:CONTENT_FONT_SIZE
-//                                            constrainedToSize:CGSizeMake(kContentLength, FLT_MAX)];
-//    self.contentLabel.frame = CGRectMake(self.headView.left, self.headView.bottom + CELL_PADDING_10,
-//                                        kContentLength, contentSize.height);
+    CGFloat kTitleLength = self.contentView.width - contentViewMarin * 2;
+    CGSize titleSize = [self.topicTitleLabel.text sizeWithFont:TITLE_FONT_SIZE
+                                            constrainedToSize:CGSizeMake(kTitleLength, FLT_MAX)];
+    self.topicTitleLabel.frame = CGRectMake(self.headView.left, self.headView.bottom + CELL_PADDING_4,
+                                        kTitleLength, titleSize.height);
+    
+    self.lastRepliedLabel.frame = CGRectMake(self.topicTitleLabel.left, self.topicTitleLabel.bottom + CELL_PADDING_4,
+                                             self.topicTitleLabel.width, self.lastRepliedLabel.font.lineHeight);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -164,11 +174,16 @@
         else {
             [self.headView setPathToNetworkImage:nil];
         }
-        self.textLabel.text = o.topicTitle;
-        NSLog(@"%@", self.detailTextLabel);
-        self.detailTextLabel.text = [NSString stringWithFormat:@"%@•%@•创建于%@",
-                                     o.nodeName, o.user.username, [o.repliedAtDate formatRelativeTime]];// dynamic calculate relative time
-        //self.contentLabel.text = o.text;
+        self.textLabel.text = o.user.username;
+        self.detailTextLabel.text = [o.createdAtDate formatRelativeTime];
+        self.topicTitleLabel.text = o.topicTitle;
+        if (o.lastRepliedUser.username) {
+            self.lastRepliedLabel.text = [NSString stringWithFormat:@"%@•最后由%@于%@前回复",
+                                          o.nodeName, o.lastRepliedUser.username, [o.repliedAtDate formatRelativeTime]];
+        }
+        else {
+            self.lastRepliedLabel.text = [NSString stringWithFormat:@"%@•还没有回复，快进去讨论吧！", o.nodeName];
+        }
     }
     return YES;
 }
