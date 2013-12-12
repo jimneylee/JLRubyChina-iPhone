2f8626567bff071726ce843515e93131a7bac406
jimneylee <jimney@163.com>
2013-12-12 17:13:38 +0800

    finish cool sites show

---
+++ JLRubyChina/Controller/RCCoolSitesC.h
@@ -0,0 +1,13 @@
+//
+//  RCCoolSitesC.h
+//  JLRubyChina
+//
+//  Created by ccjoy-jimneylee on 13-12-12.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCCoolSitesC : UITableViewController
+
+@end
