dd5ff3be063f168730880ef4437ae593efe81f3a
jimneylee <jimney@163.com>
2013-12-14 23:30:58 +0800

    update

--- JLRubyChina/Util/SMNetworkSpy.m
+++ JLRubyChina/Util/SMNetworkSpy.m
@@ -111,7 +111,7 @@ static SMNetworkSpy* sharedNetworkSpy = nil;
         title = @"当前2g/3g已连接";
     }
     
-    [RCGlobalConfig hudShowMessage:title addedToView:[UIApplication sharedApplication].keyWindow];
+    [RCGlobalConfig HUDShowMessage:title addedToView:[UIApplication sharedApplication].keyWindow];
 }
 
 @end
