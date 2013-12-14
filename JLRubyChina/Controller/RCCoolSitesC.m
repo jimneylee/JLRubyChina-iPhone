8de9bd24d5bf3853d86668efbca5758d2e74581a
jimneylee <jimney@163.com>
2013-12-14 23:33:46 +0800

    update

--- JLRubyChina/Controller/RCCoolSitesC.m
+++ JLRubyChina/Controller/RCCoolSitesC.m
@@ -77,6 +77,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
