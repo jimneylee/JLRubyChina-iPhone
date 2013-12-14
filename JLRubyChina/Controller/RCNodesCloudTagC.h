ee276f509e1f7b73958f3e015531cc6fb83bb7d4
jimneylee <jimney@163.com>
2013-12-14 23:29:30 +0800

    change name

---
+++ JLRubyChina/Controller/RCNodesCloudTagC.h
@@ -0,0 +1,21 @@
+//
+//  RCForumNodesCloudTagC.h
+//  JLRubyChina
+//
+//  Created by ccjoy-jimneylee on 13-12-13.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
+//
+
+#import "RCNodeEntity.h"
+
+@protocol RCNodesCloudTagDelegate;
+@interface RCNodesCloudTagC : UIViewController
+@property (nonatomic, assign) id<RCNodesCloudTagDelegate>delegate;
+@end
+
+@protocol RCNodesCloudTagDelegate <NSObject>
+
+@optional
+- (void)didSelectANode:(RCNodeEntity*)nodeEntity;
+
+@end
