aa3aa567b686f283db70682e331c1a51fe7ee95c
jimneylee <jimney@163.com>
2013-12-14 23:37:06 +0800

    add nimbusImageNamed

---
+++ JLRubyChina/Categories/UIImage+nimbusImageNamed.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+nimbusImageNamed.h
+//  SinaMBlogNimbus
+//
+//  Created by Lee jimney on 10/7/13.
+//  Copyright (c) 2013 jimneylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (nimbusImageNamed)
+
++ (UIImage*)nimbusImageNamed:(NSString*)imageName;
+
+@end
