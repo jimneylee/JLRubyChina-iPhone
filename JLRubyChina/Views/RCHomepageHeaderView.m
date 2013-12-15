70391f4810babc719726b159205bc8a818529a78
jimneylee <jimney@163.com>
2013-12-15 21:15:32 +0800

    添加用户的已发帖子和收藏帖子查看按钮

--- JLRubyChina/Views/RCHomepageHeaderView.m
+++ JLRubyChina/Views/RCHomepageHeaderView.m
@@ -11,21 +11,29 @@
 #import "UIImage+nimbusImageNamed.h"
 #import "UIView+findViewController.h"
 #import "RCUserHomepageC.h"
+#import "RCForumTopicsC.h"
 #import "RCUserFullEntity.h"
 
 #define NAME_FONT_SIZE [UIFont boldSystemFontOfSize:22.f]
 #define LOGIN_ID_FONT_SIZE [UIFont systemFontOfSize:16.f]
 #define TAG_LINE_ID_FONT_SIZE [UIFont systemFontOfSize:12.f]
+#define BUTTON_FONT_SIZE [UIFont boldSystemFontOfSize:15.f]
 
 #define HEAD_IAMGE_HEIGHT 60
+#define BUTTON_SIZE CGSizeMake(104.f, 40.f)
 
 @interface RCHomepageHeaderView()
+@property (nonatomic, strong) RCUserFullEntity* user;
+
 @property (nonatomic, strong) UIView* contentView;
 @property (nonatomic, strong) UILabel* nameLabel;
 @property (nonatomic, strong) UILabel* loginIdLabel;
 @property (nonatomic, strong) UILabel* tagLineLabel;
 @property (nonatomic, strong) NINetworkImageView* headView;
-@property (nonatomic, strong) RCUserFullEntity* user;
+
+@property (nonatomic, strong) UIButton* detailBtn;
+@property (nonatomic, strong) UIButton* postedTopicsBtn;
+@property (nonatomic, strong) UIButton* favoritedTopicsBtn;
 @end
 
 @implementation RCHomepageHeaderView
@@ -77,7 +85,7 @@
         self.contentView.layer.borderWidth = 1.0f;
         
         self.backgroundColor = [UIColor clearColor];
-        self.contentView.backgroundColor = CELL_CONTENT_VIEW_BG_COLOR;
+        self.contentView.backgroundColor = [UIColor whiteColor];//CELL_CONTENT_VIEW_BG_COLOR;
         self.nameLabel.backgroundColor = [UIColor clearColor];
         self.loginIdLabel.backgroundColor = [UIColor clearColor];
         self.tagLineLabel.backgroundColor = [UIColor clearColor];
@@ -123,11 +131,22 @@
     // bottom margin
     height = height + sideMargin;
     
+    // botton height
+    height = height + BUTTON_SIZE.height;
+    
     // content view
     self.contentView.frame = CGRectMake(cellMargin, cellMargin, kContentMaxWidth, height - cellMargin * 2);
     
     // self height
     self.height = height;
+    
+    // button
+    self.detailBtn.left = 0.f;
+    self.detailBtn.bottom = self.contentView.height;
+    self.postedTopicsBtn.left = self.detailBtn.right;
+    self.postedTopicsBtn.bottom = self.detailBtn.bottom;
+    self.favoritedTopicsBtn.left = self.postedTopicsBtn.right;
+    self.favoritedTopicsBtn.bottom = self.postedTopicsBtn.bottom;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -141,7 +160,7 @@
         else {
             [self.headView setPathToNetworkImage:nil];
         }
-        self.nameLabel.text = user.name;
+        self.nameLabel.text = user.name.length ? user.name : user.loginId;// if no name, just set loginId
         self.loginIdLabel.text = user.loginId;
         self.tagLineLabel.text = [NSString stringWithFormat:@"签名：%@", user.tagline.length ? user.tagline : @"暂无"];
     }
@@ -157,6 +176,100 @@
         RCUserHomepageC* c = [[RCUserHomepageC alloc] initWithUserLoginId:self.user.loginId];
         [superviewC.navigationController pushViewController:c animated:YES];
     }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIButton Action
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)showUserDetailAction
+{
+    // TODO:
+    [RCGlobalConfig HUDShowMessage:@"to do it!"
+                       addedToView:[UIApplication sharedApplication].keyWindow];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)showPostedTopicsAction
+{
+    UIViewController* superviewC = self.viewController;
+    if (superviewC) {
+        RCForumTopicsC* c = [[RCForumTopicsC alloc] initWithUserLoginId:self.user.loginId];
+        [superviewC.navigationController pushViewController:c animated:YES];
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)showFavoritedTopicsAction
+{
+    UIViewController* superviewC = self.viewController;
+    if (superviewC) {
+        RCForumTopicsC* c = [[RCForumTopicsC alloc] initForFavoritedWithUserLoginId:self.user.loginId];
+        [superviewC.navigationController pushViewController:c animated:YES];
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - View init
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (UIButton*)detailBtn
+{
+    if (!_detailBtn) {
+        _detailBtn = [[UIButton alloc] initWithFrame:CGRectMake(0.f, 0.f,
+                                                                BUTTON_SIZE.width, BUTTON_SIZE.height)];
+        [_detailBtn.titleLabel setFont:BUTTON_FONT_SIZE];
+        [_detailBtn setTitle:@"资料" forState:UIControlStateNormal];
+        [_detailBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_detailBtn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+        [_detailBtn addTarget:self action:@selector(showUserDetailAction)
+             forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:_detailBtn];
+        _detailBtn.layer.borderColor = CELL_CONTENT_VIEW_BORDER_COLOR.CGColor;
+        _detailBtn.layer.borderWidth = 1.0f;
+    }
+    return _detailBtn;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (UIButton*)postedTopicsBtn
+{
+    if (!_postedTopicsBtn) {
+        _postedTopicsBtn = [[UIButton alloc] initWithFrame:CGRectMake(0.f, 0.f,
+                                                                      BUTTON_SIZE.width, BUTTON_SIZE.height)];
+        [_postedTopicsBtn.titleLabel setFont:BUTTON_FONT_SIZE];
+        [_postedTopicsBtn setTitle:@"帖子" forState:UIControlStateNormal];
+        [_postedTopicsBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_postedTopicsBtn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+        [_postedTopicsBtn addTarget:self action:@selector(showPostedTopicsAction)
+                   forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:_postedTopicsBtn];
+        _postedTopicsBtn.layer.borderColor = CELL_CONTENT_VIEW_BORDER_COLOR.CGColor;
+        _postedTopicsBtn.layer.borderWidth = 1.0f;
+    }
+    return _postedTopicsBtn;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (UIButton*)favoritedTopicsBtn
+{
+    if (!_favoritedTopicsBtn) {
+        _favoritedTopicsBtn = [[UIButton alloc] initWithFrame:CGRectMake(0.f, 0.f,
+                                                                         BUTTON_SIZE.width, BUTTON_SIZE.height)];
+        [_favoritedTopicsBtn.titleLabel setFont:BUTTON_FONT_SIZE];
+        [_favoritedTopicsBtn.titleLabel setTextColor:[UIColor grayColor]];
+        [_favoritedTopicsBtn setTitle:@"收藏" forState:UIControlStateNormal];
+        [_favoritedTopicsBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_favoritedTopicsBtn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+        [_favoritedTopicsBtn addTarget:self action:@selector(showFavoritedTopicsAction)
+                      forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:_favoritedTopicsBtn];
+        _favoritedTopicsBtn.layer.borderColor = CELL_CONTENT_VIEW_BORDER_COLOR.CGColor;
+        _favoritedTopicsBtn.layer.borderWidth = 1.0f;
+    }
+    return _favoritedTopicsBtn;
 }
 
 @end
