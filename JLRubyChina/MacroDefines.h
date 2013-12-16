16fed94bb541e4697efaad7496e9db22677eaec5
jimneylee <jimney@163.com>
2013-12-16 23:47:10 +0800

    move RCGlobalConfig's macro define to this file, better

---
+++ JLRubyChina/MacroDefines.h
@@ -0,0 +1,59 @@
+//
+//  MacroDefines.h
+//  JLRubyChina
+//
+//  Created by Lee jimney on 12/16/13.
+//  Copyright (c) 2013 jimneylee. All rights reserved.
+//
+
+#ifndef JLRubyChina_MacroDefines_h
+#define JLRubyChina_MacroDefines_h
+
+// 测试access_token: @jimneylee
+#if DEBUG
+#define MY_ACCESS_TOKEN_TEST @"8a67b1e1042c8093f709:4988"
+#define MY_LOGIN_ID_TEST @"jimneylee"
+#endif
+
+#define APP_NAME [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"]
+#define APP_VERSION [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+#define APP_ID [[[NSBundle mainBundle] objectForInfoDictionaryKey:@"APP_STORE_ID"] longValue]
+#define IOS_7_X (([[UIDevice currentDevice].systemVersion floatValue] > 6.99))
+
+// iOS 系统版本
+#define IOS_IS_AT_LEAST_6 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0)
+#define IOS_IS_AT_LEAST_7 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+
+// 是否是iphone5的判断
+#define IS_WIDESCREEN ([[UIScreen mainScreen] bounds].size.height > 500 )
+#define IS_IPHONE ( [[[UIDevice currentDevice] model] isEqualToString:@"iPhone"] \
+|| [[[UIDevice currentDevice] model] isEqualToString:@"iPhone Simulator"])
+#define IS_IPHONE5 (IS_IPHONE && IS_WIDESCREEN)
+
+#define INVALID_INDEX -1
+#define CELL_PADDING_10 10
+#define CELL_PADDING_8 8
+#define CELL_PADDING_6 6
+#define CELL_PADDING_4 4
+#define CELL_PADDING_2 2
+#define PHONE_SCREEN_WIDTH 320
+
+#define APP_THEME_COLOR RGBCOLOR(41, 41, 41)
+#define RUBY_RED_COLOR RGBCOLOR(177, 9, 0)
+#define TABLE_VIEW_BG_COLOR RGBCOLOR(230, 230, 230)
+#define CELL_CONTENT_VIEW_BG_COLOR RGBCOLOR(247, 247, 247)
+#define CELL_CONTENT_VIEW_BORDER_COLOR RGBCOLOR(234, 234, 234)
+#define SIDE_DIRECTION_LEFT_OFFSET 160//左边栏tableview离右边距离，理解有点别扭，后面简洁化
+
+#define RC_WIKI_URL @"http://ruby-china.org/wiki"
+
+// 自定义链接协议
+#define PROTOCOL_AT_SOMEONE @"atsomeone://"
+#define PROTOCOL_SHARP_FLOOR @"sharpfloor://"
+#define PROTOCOL_NODE @"node://"
+
+// notification
+#define DID_LOGIN_NOTIFICATION @"DID_LOGIN_NOTIFICATION"
+#define DID_LOGOUT_NOTIFICATION @"DID_LOGOUT_NOTIFICATION"
+
+#endif
