7a08ac14aae0971e0835edf64de9950e1c644b39
jimneylee <jimney@163.com>
2013-12-25 20:40:47 +0800

    CONTENT_FONT_SIZE -> STHeitiSC-Light

--- JLRubyChina/Views/RCReplyCell.m
+++ JLRubyChina/Views/RCReplyCell.m
@@ -19,8 +19,7 @@
 
 #define NAME_FONT_SIZE [UIFont systemFontOfSize:15.f]
 #define DATE_FONT_SIZE [UIFont systemFontOfSize:12.f]
-// 冬青字体：http://tadaland.com/ios-better-experience-font-hiragino.html
-#define CONTENT_FONT_SIZE [UIFont fontWithName:@"Hiragino Sans GB" size:17.f]
+#define CONTENT_FONT_SIZE [UIFont fontWithName:@"STHeitiSC-Light" size:17.f]
 #define BUTTON_FONT_SIZE [UIFont boldSystemFontOfSize:13.f]
 
 #define CONTENT_LINE_HEIGHT 21.f
