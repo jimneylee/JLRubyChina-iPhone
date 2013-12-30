dbe6c4d224228e40153be0b78ecfe7cbb8898f8c
Ken_lu <luzhiyongmail@sohu.com>
2013-12-30 10:50:52 +0800

    add the paging feature

---
+++ JLRubyChina/Entities/RCNodeSectionEntity.m
@@ -0,0 +1,22 @@
+//
+//  RCNodeSectionEntity.m
+//  JLRubyChina
+//
+//  Created by Ken_lu on 12/26/13.
+//  Copyright (c) 2013 jimneylee. All rights reserved.
+//
+
+#import "RCNodeSectionEntity.h"
+#import "RCNodeEntity.h"
+
+@implementation RCNodeSectionEntity
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
