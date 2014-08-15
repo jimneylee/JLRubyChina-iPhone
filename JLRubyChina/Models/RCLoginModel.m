ef3b4a6554a10f71ae8a24909a42ec3c04a85739
jimneylee <jimneylee@gmail.com>
2014-08-15 14:25:49 +0800

    finish login using afnetworking2.x

--- JLRubyChina/Models/RCLoginModel.m
+++ JLRubyChina/Models/RCLoginModel.m
@@ -21,40 +21,37 @@
                     block:(void(^)(RCAccountEntity* user, NSError *error))block
 {
     NSString* path = [RCAPIClient relativePathForSignIn];
-#if 0
+#if 1
     // 由于登录的接口与其他接口base_url不太一样，后台没有放到api路径下，故单独处理
     // ruby-china.org/account/sign_in.json
-    AFHTTPRequestOperationManager *httpClient = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:HOST_URL]];//@"http://ruby-china.org/"
-//    [httpClient setParameterEncoding:AFJSONParameterEncoding];
-    [httpClient setAuthorizationHeaderWithUsername:username password:password];
-//    [httpClient registerHTTPOperationClass:[AFJSONRequestOperation class]];
-    // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
-//	[httpClient setDefaultHeader:@"Accept" value:@"application/json"];
-    [httpClient postPath:path parameters:nil
-                                 success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                                     if ([responseObject isKindOfClass:[NSDictionary class]]) {
-                                         RCAccountEntity* account = [RCAccountEntity entityWithDictionary:responseObject];
-                                         [RCGlobalConfig setMyLoginId:account.loginId];
-                                         [RCGlobalConfig setMyToken:account.privateToken];
-                                         [RCAccountEntity storePrivateToken:account.privateToken forLoginId:account.loginId];
-                                         if (block) {
-                                             block(account, nil);
-                                         }
-                                     }
-                                     else {
-                                         if (block) {
-                                             NSError* error = [[NSError alloc] init];
-                                             block(nil, error);
-                                         }
-                                     }
-                                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                                     //NSDictionary* info = [error.userInfo objectForKey:@"NSLocalizedRecoverySuggestion"];
-                                     //NSLog(@"error: %@", [info objectForKey:@"error"]);
-                                     //NSLog(@"error: %@", error);
-                                     if (block) {
-                                         block(nil, error);
-                                     }
-                                 }];
+    AFHTTPRequestOperationManager *httpClient = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:
+                                                 [NSURL URLWithString:HOST_URL]];
+    [httpClient.requestSerializer setAuthorizationHeaderFieldWithUsername:username password:password];
+    [httpClient POST:path parameters:nil
+             success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                 if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                     RCAccountEntity* account = [RCAccountEntity entityWithDictionary:responseObject];
+                     [RCGlobalConfig setMyLoginId:account.loginId];
+                     [RCGlobalConfig setMyToken:account.privateToken];
+                     [RCAccountEntity storePrivateToken:account.privateToken forLoginId:account.loginId];
+                     if (block) {
+                         block(account, nil);
+                     }
+                 }
+                 else {
+                     if (block) {
+                         NSError* error = [[NSError alloc] init];
+                         block(nil, error);
+                     }
+                 }
+             } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                 //NSDictionary* info = [error.userInfo objectForKey:@"NSLocalizedRecoverySuggestion"];
+                 //NSLog(@"error: %@", [info objectForKey:@"error"]);
+                 //NSLog(@"error: %@", error);
+                 if (block) {
+                     block(nil, error);
+                 }
+             }];
 #endif
 }
 
