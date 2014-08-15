b50bdde400430d4f0755398025698b239462d0c6
jimneylee <jimney@163.com>
2014-08-15 15:28:50 +0800

    change author to jimneylee

--- JLRubyChina/Entities/RCKeywordEntity.m
+++ JLRubyChina/Entities/RCKeywordEntity.m
@@ -2,7 +2,7 @@
 //  RCKeywordsEntity.m
 //  JLRubyChina
 //
-//  Created by ccjoy-jimneylee on 13-12-11.
+//  Created by jimneylee on 13-12-11.
 //  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
--- JLRubyChina/Entities/RCUserEntity.h
+++ JLRubyChina/Entities/RCUserEntity.h
@@ -2,7 +2,7 @@
 //  RCUserEntity.h
 //  JLRubyChina
 //
-//  Created by ccjoy-jimneylee on 13-12-10.
+//  Created by jimneylee on 13-12-10.
 //  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
