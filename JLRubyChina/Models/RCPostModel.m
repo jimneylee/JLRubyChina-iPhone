86f46a02023a60e9a3648d4d58aaa577c8875d66
jimneylee <jimneylee@gmail.com>
2014-08-15 14:25:40 +0800

    finish post using afnetworking2.x

--- JLRubyChina/Models/RCPostModel.m
+++ JLRubyChina/Models/RCPostModel.m
@@ -13,15 +13,12 @@
 @implementation RCPostModel
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-- (void)old_postNewTopicWithTitle:(NSString*)title
+- (void)postNewTopicWithTitle:(NSString*)title
                              body:(NSString*)body
                            nodeId:(NSUInteger)nodeId
                           success:(void(^)(RCTopicEntity* topicEntity))success
                           failure:(void(^)(NSError *error))failure
 {
-#warning "to use af2.0"
-#if 0
-    // 不知道这么为什么不行，下面替代方法临时实现，比较丑陋
     if (title.length && body.length && nodeId > 0) {
         NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
         [parameters setObject:title forKey:@"title"];
@@ -30,76 +27,27 @@
         [parameters setObject:[RCGlobalConfig myToken] forKey:@"token"];
         
         NSString* path = [RCAPIClient relativePathForPostNewTopic];
-        [[RCAPIClient sharedClient] postPath:path parameters:parameters
+        [[RCAPIClient sharedClient] POST:path parameters:parameters
                                      success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                                         NSLog(@"%@", responseObject);
-                                         success(nil);
+
+                                         BOOL successFlag = NO;
+                                         if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                                             RCTopicEntity* topicEntity = [RCTopicEntity entityWithDictionary:responseObject];
+                                             if (topicEntity) {
+                                                 success(topicEntity);
+                                                 successFlag = YES;
+                                             }
+                                         }
+                                         
+                                         if (!successFlag) {
+                                             failure(nil);
+                                         }
                                      }
                                      failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                                          NSLog(@"%@", error);
                                          failure(error);
                                      }];
     }
-#endif
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-- (void)postNewTopicWithTitle:(NSString*)title
-                         body:(NSString*)body
-                       nodeId:(NSUInteger)nodeId
-                      success:(void(^)(RCTopicEntity* topicEntity))success
-                      failure:(void(^)(NSError *error))failure
-{
-#warning "to use af2.0"
-#if 0
-    // 参考：http://stackoverflow.com/questions/9562459/afnetworking-posting-malformed-json-single-quotes-and-object-refs
-    if (title.length && body.length && nodeId > 0) {
-        NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
-        [parameters setObject:title forKey:@"title"];
-        [parameters setObject:body forKey:@"body"];
-        [parameters setObject:[NSNumber numberWithUnsignedInt:nodeId] forKey:@"node_id"];
-        [parameters setObject:[RCGlobalConfig myToken] forKey:@"token"];
-        
-        NSString* path = [RCAPIClient relativePathForPostNewTopic];
-        NSError *error = nil;
-        NSData* jsonData = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error:&error];
-        
-        AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:HOST_API_URL]];
-        [httpClient setParameterEncoding:AFFormURLParameterEncoding];
-        NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST"
-                                                                path:path
-                                                          parameters:nil];
-        NSMutableData *body = [NSMutableData data];
-        [body appendData:jsonData];
-        [request setHTTPBody:body];
-        [request setHTTPMethod:@"POST"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
-        
-        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-        [httpClient registerHTTPOperationClass:[AFHTTPRequestOperation class]];
-        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-            NSString* resultString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
-            NSLog(@"Response: %@", resultString);
-            NSError *error = nil;
-            id responseJSON  = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:&error];
-            if ([responseJSON isKindOfClass:[NSDictionary class]]) {
-                RCTopicEntity* topicEntity = [RCTopicEntity entityWithDictionary:responseJSON];
-                if (topicEntity) {
-                    success(topicEntity);
-                    return;
-                }
-                else {
-                    NSLog(@"error:%@", error);
-                }
-            }
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            NSLog(@"Error: %@", error);
-            failure(nil);
-        }];
-        [operation start];
-    }
-#endif
-
-}
 @end
